fb62a200622c053118b4769f3420457d95a020fe
aaronteamAAR <rooneychris83@gmail.com>
2024-01-21 18:41:48 +0100

    ADDED TS

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = "Greenlife"
+export const SITE_DESCRIPTION = "Health and wellness articles and product recommendation"
+export const HOMEPAGE_ARTICLE_LIMIT = 7
+export const ARTICLE_PER_PAGE = 6
