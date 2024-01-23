746d13c93fe189eeb15c08293412a86986e1ba7f
aaronteamAAR <rooneychris83@gmail.com>
2024-01-23 07:11:38 +0100

    Refix footer

--- src/utils.ts
+++ src/utils.ts
@@ -8,6 +8,12 @@ function formDate(date: Date): string {
   return new Date(date).toLocaleDateString(undefined, options);
 }
 
+// Tags to UpperCase
+function captialize(str: string):string {
+  if (typeof str !== 'string' || str.length === 0){
+    return str
+  }
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
 
-
-export { formDate }
+export { formDate, captialize }
